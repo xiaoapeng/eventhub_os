b4b1716e434754b9b55abd8bd7843545ad18aa0e
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-07-27 10:04:06 +0800

    添加ringbuf实现（待测）

---
+++ src/general/eh_ringbuf.c
@@ -0,0 +1,146 @@
+/**
+ * @file eh_ringbuf.c
+ * @brief   环形缓冲区实现，单读单写无锁环形缓冲区
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-07-23
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "eh_types.h"
+#include "eh_error.h"
+#include "eh_mem.h"
+#include "eh_ringbuf.h"
+
+
+#define eh_ringbuf_fix(ringbuf, pos)	        ((pos)%((uint32_t)(ringbuf->size << 1)))
+
+eh_ringbuf_t* eh_ringbuf_create(int32_t size, uint8_t *static_buf_or_null){
+    eh_ringbuf_t* ringbuf;
+    if(size <= 0)
+        return eh_error_to_ptr(EH_RET_INVALID_PARAM);
+    if(static_buf_or_null == NULL){
+        ringbuf = eh_malloc(sizeof(eh_ringbuf_t) + (size_t)size);
+        static_buf_or_null = (uint8_t*)(ringbuf + 1);
+    }else{
+        ringbuf = eh_malloc(sizeof(eh_ringbuf_t));
+    }
+    if(ringbuf == NULL)
+        return eh_error_to_ptr(EH_RET_MALLOC_ERROR);
+    ringbuf->buf = static_buf_or_null;
+    ringbuf->r = ringbuf->w = 0;
+    ringbuf->size = size;
+    return ringbuf;
+}
+
+void eh_ringbuf_destroy(eh_ringbuf_t *ringbuf){
+    eh_free(ringbuf);
+}
+
+int32_t eh_ringbuf_size(eh_ringbuf_t *ringbuf){
+    uint32_t w = ringbuf->w;
+    uint32_t r = ringbuf->r;
+    int32_t diff = (int)(w - r);
+    return diff >=0 ? diff : (ringbuf->size << 1) + diff;
+}
+
+int32_t eh_ringbuf_free_size(eh_ringbuf_t *ringbuf){
+    uint32_t w = ringbuf->w;
+    uint32_t r = ringbuf->r;
+    int32_t diff = (int)(w - r);
+    return diff >=0 ? ringbuf->size - diff : (- (ringbuf->size + diff));
+}
+
+
+int32_t eh_ringbuf_write(eh_ringbuf_t *ringbuf, const uint8_t *buf, int32_t len){
+    uint32_t w;
+    int32_t free_size = eh_ringbuf_free_size(ringbuf);
+    int32_t write_size_first_max,wl;
+    wl = len > free_size ? free_size : len;
+    if(wl <= 0) return 0;
+    w = ringbuf->w % (uint32_t)ringbuf->size;
+    write_size_first_max = ringbuf->size - (int32_t)w;
+    if(wl <= write_size_first_max){
+        memcpy(ringbuf->buf + w, buf, (size_t)wl);
+    }else{
+        memcpy(ringbuf->buf + w, buf, (size_t)write_size_first_max);
+        memcpy(ringbuf->buf, buf + write_size_first_max, (size_t)(wl - write_size_first_max));
+    }
+    eh_memory_order_release_barrier();
+    ringbuf->w = eh_ringbuf_fix(ringbuf, ringbuf->w + (uint32_t)wl);
+    return wl;
+}
+
+int32_t eh_ringbuf_read(eh_ringbuf_t *ringbuf, uint8_t *buf, int32_t len){
+    uint32_t r;
+    int32_t size = eh_ringbuf_size(ringbuf);
+    int32_t read_size_first_max, rl;
+    rl = len > size ? size : len;
+    if(rl <= 0) return 0;
+    r = ringbuf->r % (uint32_t)ringbuf->size;
+    read_size_first_max = ringbuf->size - (int32_t)r;
+    if(rl <= read_size_first_max){
+        memcpy(buf, ringbuf->buf + r, (size_t)rl);
+    }else{
+        memcpy(buf, ringbuf->buf + r, (size_t)read_size_first_max);
+        memcpy(buf + read_size_first_max, ringbuf->buf, (size_t)(rl - read_size_first_max));
+    }
+    
+    eh_memory_order_release_barrier();
+    ringbuf->r = eh_ringbuf_fix(ringbuf, ringbuf->r + (uint32_t)rl);
+    return rl;
+}
+
+int32_t eh_ringbuf_read_skip(eh_ringbuf_t *ringbuf, int32_t len){
+    int32_t size = eh_ringbuf_size(ringbuf);
+    int32_t rl;
+    rl = len > size ? size : len;
+    if(rl <= 0) return 0;
+    eh_memory_order_release_barrier();
+    ringbuf->r = eh_ringbuf_fix(ringbuf, ringbuf->r + (uint32_t)rl);
+    return rl;
+}
+
+
+const uint8_t* eh_ringbuf_peek(eh_ringbuf_t *ringbuf, int32_t offset, uint8_t *buf, int32_t *len){
+    uint32_t r;
+    int32_t size;
+    int32_t read_size_first_max;
+    int32_t rl;
+
+    /* 为了性能，这里不进行负数的判断 */
+
+    size = eh_ringbuf_size(ringbuf) - offset;
+    rl = *len;
+    if(size < rl ) return NULL; /* 数量不足，禁止偷看 */
+    r = (ringbuf->r + (uint32_t)offset)%(uint32_t)ringbuf->size;
+    read_size_first_max = ringbuf->size - (int32_t)r;
+
+    if(rl <= read_size_first_max){
+        /* 0拷贝情况，皆大欢喜 */
+        *len = size > read_size_first_max ? read_size_first_max : size;
+        return ringbuf->buf + r;
+    }else{
+        memcpy(buf, ringbuf->buf + r, (size_t)read_size_first_max);
+        memcpy(buf + read_size_first_max, ringbuf->buf, (size_t)(rl - read_size_first_max));
+        return buf;
+    }
+}
+
+void eh_ringbuf_clear(eh_ringbuf_t *ringbuf){
+    eh_memory_order_release_barrier();
+    ringbuf->r = ringbuf->w;
+}
+
+
+
+
+
+
+
---
+++ src/general/include/eh_ringbuf.h
@@ -0,0 +1,120 @@
+/**
+ * @file eh_ringbuf.h
+ * @brief 环形缓冲区实现，单读单写无锁环形缓冲区
+ * @author simon.xiaoapeng (simon.xiaoapeng@gmail.com)
+ * @version 1.0
+ * @date 2024-07-23
+ * 
+ * @copyright Copyright (c) 2024  simon.xiaoapeng@gmail.com
+ * 
+ * @par 修改日志:
+ */
+#ifndef _EH_RINGBUF_H_
+#define _EH_RINGBUF_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+
+typedef struct eh_ringbuf{
+    uint32_t w;
+    uint32_t r;
+    int32_t  size;
+    uint8_t *buf;
+}eh_ringbuf_t;
+
+/**
+ * @brief                           创建环形缓冲区
+ * @param  size                     环形缓冲区大小,要求在正数范围内，因为使用镜像法缓冲区
+                                    模块中所有的int32_t都是提醒使用者注意范围
+ * @param  static_buf_or_null       静态缓冲区指针，如果为NULL则动态分配内存
+ * @return eh_ringbuf_t*            返回值请使用eh_ptr_to_error来判断是否创建成功
+ */
+extern eh_ringbuf_t* eh_ringbuf_create(int32_t size, uint8_t *static_buf_or_null);
+
+/**
+ * @brief                           销毁环形缓冲区
+ * @param  ringbuf                  环形缓冲区指针
+ */
+extern void eh_ringbuf_destroy(eh_ringbuf_t *ringbuf);
+
+/**
+ * @brief                           获取环形缓冲区剩余空间
+ * @param  ringbuf                  环形缓冲区指针
+ * @return int32_t                 返回剩余空间
+ */
+extern int32_t eh_ringbuf_free_size(eh_ringbuf_t *ringbuf);
+
+/**
+ * @brief                           获取环形缓冲区已用大小
+ * @param  ringbuf                  环形缓冲区指针
+ * @return int32_t                 返回环形缓冲区已用大小
+ */
+extern int32_t eh_ringbuf_size(eh_ringbuf_t *ringbuf);
+
+/**
+ * @brief                           获取环形缓冲区总量
+ * @param  ringbuf                  环形缓冲区指针
+ * @return int32_t                 返回环形缓冲区总量
+ */
+static inline int32_t eh_ringbuf_total_size(eh_ringbuf_t *ringbuf){
+    return ringbuf->size;
+}
+
+/**
+ * @brief                           写入环形缓冲区
+ * @param  ringbuf                  环形缓冲区指针
+ * @param  buf                      缓冲区指针
+ * @param  len                      要写的环形缓冲区长度
+ * @return int32_t                  返回写入成功的数量
+ */
+extern int32_t eh_ringbuf_write(eh_ringbuf_t *ringbuf, const uint8_t *buf, int32_t len);
+
+/**
+ * @brief                           读取环形缓冲区
+ * @param  ringbuf                  环形缓冲区指针
+ * @param  buf                      要读到的缓冲区指针
+ * @param  len                      要读的环形缓冲区长度
+ * @return int32_t                  返回读到的数量
+ */
+extern int32_t eh_ringbuf_read(eh_ringbuf_t *ringbuf, uint8_t *buf, int32_t len);
+
+/**
+ * @brief                           跳过读环形缓冲区
+ * @param  ringbuf                  环形缓冲区指针
+ * @param  len                      要跳过的环形缓冲区长度
+ * @return int32_t                  返回跳过的数量
+ */
+extern int32_t eh_ringbuf_read_skip(eh_ringbuf_t *ringbuf, int32_t len);
+
+/**
+ * @brief                           偷看环形缓冲区
+ * @param  ringbuf                  环形缓冲区指针
+ * @param  offset                   从哪开始偷看
+ * @param  buf                      要读到的缓冲区指针
+ * @param  len                      作为输入参数时为buf的长度，作为输出参数时为访问uint8_t 的长度
+ * @return const uint8_t*           当前缓冲区没有足够数量时返回NULL
+ *                                  当数量足够且需要绕回时进行memcpy后返回buf
+ *                                  当数量足够且不需要绕回时直接返回内部缓冲区指针(0拷贝)
+ */
+extern const uint8_t* eh_ringbuf_peek(eh_ringbuf_t *ringbuf, int32_t offset, uint8_t *buf, int32_t *len);
+
+/**
+ * @brief                           清空环形缓冲区
+ * @param  ringbuf                  环形缓冲区指针
+ */
+extern void eh_ringbuf_clear(eh_ringbuf_t *ringbuf);
+
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
+
+
+#endif // _EH_RINGBUF_H_
