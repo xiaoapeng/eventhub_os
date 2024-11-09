1fdd8a5d891b8c83de9b7f5e711fe60f7221a26c
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-11-09 20:32:38 +0800

    refactor(eh_error): 优化错误处理宏和函数
    - 修改错误代码定义，统一格式
    - 重写 eh_ptr_to_error 函数，提高代码可读性和性能
    - 添加 likely 属性，帮助编译器优化分支预测

--- src/include/eh_error.h
+++ src/include/eh_error.h
@@ -11,6 +11,7 @@
 #ifndef _EH_ERROR_H_
 #define _EH_ERROR_H_
 
+#include "eh_types.h"
 
 #ifdef __cplusplus
 #if __cplusplus
@@ -18,31 +19,33 @@ extern "C"{
 #endif
 #endif /* __cplusplus */
 
-#define EH_RET_OK                           0
-#define EH_RET_FAULT                        -1
-#define EH_RET_INVALID_PARAM                -2
-#define EH_RET_INVALID_STATE                -3
-#define EH_RET_BUSY                         -4
-#define EH_RET_SCHEDULING_ERROR             -5
-#define EH_RET_EVENT_ERROR                  -6
-#define EH_RET_TIMEOUT                      -7
-#define EH_RET_MALLOC_ERROR                 -8
-#define EH_RET_PTR_NULL                     -9
-#define EH_RET_MEM_POOL_EMPTY               -10
-#define EH_RET_NOT_SUPPORTED                -11
-#define EH_RET_MIN_ERROR_NUM                -255
+#define EH_RET_OK                           (0   )
+#define EH_RET_FAULT                        (-1  )
+#define EH_RET_INVALID_PARAM                (-2  )
+#define EH_RET_INVALID_STATE                (-3  )
+#define EH_RET_BUSY                         (-4  )
+#define EH_RET_SCHEDULING_ERROR             (-5  )
+#define EH_RET_EVENT_ERROR                  (-6  )
+#define EH_RET_TIMEOUT                      (-7  )
+#define EH_RET_MALLOC_ERROR                 (-8  )
+#define EH_RET_PTR_NULL                     (-9  )
+#define EH_RET_MEM_POOL_EMPTY               (-10 )
+#define EH_RET_NOT_SUPPORTED                (-11 )
+#define EH_RET_MIN_ERROR_NUM                (-255)
 
 #define eh_param_assert(condition)                                      \
     do{                                                                 \
         if(!(condition))                                                \
             return EH_RET_INVALID_PARAM;                                \
     }while(0)
-#define eh_ptr_convert_int(ptr) ((int)((long)(ptr)))
-#define eh_ptr_to_error(ptr)           ({                               \
-    (ptr) == NULL ? EH_RET_PTR_NULL :                                   \
-    (eh_ptr_convert_int(ptr)) < 0 && eh_ptr_convert_int(ptr) > EH_RET_MIN_ERROR_NUM ?           \
-    eh_ptr_convert_int(ptr) : EH_RET_OK;                                             \
-})
+
+static inline __attribute_const__ int eh_ptr_to_error(void *ptr){
+    if( eh_likely(((long)(ptr)) > 0) || eh_likely((long)(ptr) <= EH_RET_MIN_ERROR_NUM) )
+        return EH_RET_OK;
+    if(!ptr)
+        return EH_RET_PTR_NULL;
+    return (int)(long)(ptr);
+}
 
 #define eh_error_to_ptr(err_on)             ((void*)(err_on))
 
