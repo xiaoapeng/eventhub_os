71c5471b9d9a74fa4592823eed0b505827181043
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2024-11-19 22:04:31 +0800

    style: 格式化 eh_types.h 文件中的宏定义
    - 调整了 eh_aligned、eh_align_up、eh_align_down 和 eh_read_once 宏的格式

--- src/include/eh_types.h
+++ src/include/eh_types.h
@@ -78,10 +78,10 @@
 	((uintptr_t)(ptr) + offsetof(typeof(*(ptr)), member) != 0)
 
 
-#define eh_aligned(x)                        __attribute__((aligned(x)))
-#define eh_align_up(x, align) (((x) + ((align) - 1)) & (~((align) - 1)))
-#define eh_align_down(x, align) ((x) & (~((align) - 1)))
-#define eh_read_once(x)                   (*(const volatile typeof(x) *)&(x))
+#define eh_aligned(x)                           __attribute__((aligned(x)))
+#define eh_align_up(x, align)                   (((x) + ((align) - 1)) & (~((align) - 1)))
+#define eh_align_down(x, align)                 ((x) & (~((align) - 1)))
+#define eh_read_once(x)                         (*(const volatile typeof(x) *)&(x))
 
 #define __weak                              __attribute__((weak))
 #define __safety                            /* 被此宏标记的函数，可在中断和其他线程中进行调用 */
