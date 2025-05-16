836b810f8f350a65c56df51271e688c1a86a0b7f
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2025-05-16 22:19:38 +0800

    fix(debug): 修复错误信息打印格式
    - 在 EH_DBG_ERROR_EXEC 宏中调整了错误信息的打印格式

--- src/general/include/eh_debug.h
+++ src/general/include/eh_debug.h
@@ -174,11 +174,11 @@ extern int eh_dbg_hex(enum eh_dbg_level level, enum eh_dbg_flags flags, size_t l
 /**
  * @brief 打印错误原因，且执行 action 语句
  */
-#define EH_DBG_ERROR_EXEC(expression, action)  do{                              \
-    if(expression){                                                             \
-        eh_dbg_printfl(EH_DBG_ERR, "(%s) execute {%s}", #expression, #action); \
-        action;                                                                 \
-    }                                                                           \
+#define EH_DBG_ERROR_EXEC(expression, action)  do{                                 \
+    if(expression){                                                                \
+        eh_dbg_printfl(EH_DBG_ERR, "", "(%s) execute {%s}", #expression, #action); \
+        action;                                                                    \
+    }                                                                              \
 }while(0)
 
 
