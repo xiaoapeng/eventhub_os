2d35c98a00a154254a0d9504663b48fc9a1601af
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2025-08-30 17:23:43 +0800

    fix(eh_mem_pool): 调整内存池释放逻辑
    - 将 eh_enter_critical() 调用移至检查索引是否已使用之前
    - 修复了潜在的资源竞争问题

--- src/eh_mem_pool.c
+++ src/eh_mem_pool.c
@@ -90,13 +90,13 @@ void  eh_mem_pool_free(eh_mem_pool_t _pool, void* ptr){
         eh_mwarnfl(MEM_POOL, "pool index out of range pool=%p index=%d num=%d", pool, index, pool->num);
         return ;
     }
+    state = eh_enter_critical();
     if(!eh_mem_pool_idx_is_used(_pool, index)){
         /* 释放一个没有被分配的pool mem */
         eh_mwarnfl(MEM_POOL, "Release an unallocated pool. mempool=%p index=%d num=%d", pool, index, pool->num);
         return ;
     }
 
-    state = eh_enter_critical();
     pool->free_list[index].next = pool->free_list_head.next;
     pool->free_list_head.next = pool->free_list + index;
     eh_exit_critical(state);
